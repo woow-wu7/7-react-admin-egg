5606ebf4810b1d9c7c2e3afea2ee9be21ea074ba
woow_wu7 <3107075636@qq.com>
2020-10-30 20:12:23 +0800

    feat: router

--- app/router.js
+++ app/router.js
@@ -8,8 +8,8 @@ module.exports = app => {
   router.get('/', controller.home.index);
   // http://127.0.0.1:7001/user 会映射到app/controller/user.js 的index方法上
   router.resources('users', '/users', controller.users);
-  router.get('/table-list', controller.table.query);
-  router.post('/table-list/add', controller.table.add);
-  router.post('/table-list/del', controller.table.del);
-  router.post('/table-list/upd', controller.table.upd);
+  router.get('/api/table-list', controller.table.query);
+  router.post('/api/table-list/add', controller.table.add);
+  router.post('/api/table-list/del', controller.table.del);
+  router.post('/api/table-list/upd', controller.table.upd);
 };
